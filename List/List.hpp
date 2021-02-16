9898b57087fa4cd318bc8c22de89e6f208ee0bd3
ojoubout <oussamajoubouti@gmail.com>
2021-02-16 10:36:57 +0100

    overloads

--- List/List.hpp
+++ List/List.hpp
@@ -70,7 +70,7 @@ public:
     
             iterator(node_pointer ptr) : _ptr(ptr) {};
 
-            node_pointer get_node() const { return (_ptr); };
+            node_pointer get_node() { return (_ptr); };
 
             reference   operator*() { return _ptr->content; };
             pointer     operator->() { return &_ptr->content; };
@@ -83,8 +83,8 @@ public:
             // Postfix decrement
             iterator operator--(int) { iterator tmp = *this; --(*this); return tmp; }
 
-            bool    operator!=(iterator & it) { return (_ptr != it._ptr); }
-            bool    operator==(iterator & it) { return (_ptr == it._ptr); }
+            bool    operator!=(iterator const & it) { return (_ptr != it._ptr); }
+            bool    operator==(iterator const & it) { return (_ptr == it._ptr); }
 
             operator const_iterator () const { return const_iterator(_ptr) ; }
 
@@ -154,58 +154,36 @@ public:
             node_pointer _ptr;
     };
 
-
-
-    // typedef iterator const          const_iterator;
-
-
-    // List();
-    // List(size_t *n, const value_type& val);
-    // template <class InputIterator>
-    // List (InputIterator first, InputIterator last);
-    // List(const List& copy);
-    // ~List();
-
-    
-    List() : _size(0) {
-        pointer val = static_cast<pointer>(::operator new(sizeof(value_type)));
-        _end = new node(*val);
-        _begin = _end;
-        _end->prev = _end->next = NULL;
+    List() {
+        init();
     }
 
 
     List(const List & copy) {
+        init();
         *this = copy;
     }
 
+    List(size_type n, const value_type& val = value_type()) {
+        init();
+        insert(begin(), n, val);
+    }
 
-    template <typename InputIterator>
-    List(InputIterator first, InputIterator last) {
-        node_pointer tmp = _begin;
-        for (InputIterator it = first; it != last; ++it) {
-            if (tmp == _begin) {
-                _begin = new node(*it);
-                // _begin->content = *it;
-                _begin->next = NULL;
-                tmp = _begin;
-            } else {
-                tmp->next = new node(*it);
-                // tmp->next->content = *it;
-                tmp->next->next = NULL;
-                tmp = tmp->next;
-            }
-        }
+    List(const_iterator first, const_iterator last) {
+        init();
+        insert(begin(), first, last);
     }
 
 
 
     ~List() { // TODO: clear()
+        clear();
+        delete _end;
     }
 
-
     List&   operator=(const List& x) { // TODO: after finishing clear()
-        (void)x;
+        clear();
+        insert(begin(), x.begin(), x.end());
         return (*this);
     }
 
@@ -273,86 +251,236 @@ public:
     }
 
     // Modifiers:
-    void clear();
 
-    template <class InputIterator>
-    void assign (InputIterator first, InputIterator last) {
+    void assign (const_iterator first, const_iterator last) {
         clear();
-        for (InputIterator it = first; it != last; ++it) {
-            push_back(*it);
-        }
+        insert(end(), first, last);
     }
     void assign (size_type n, const value_type& val) {
         clear();
-        for (size_type i = 0; i < n; i++) {
-            push_back(val);
-        }
+        insert(end(), n, val);
     }
 
-
     void push_front (const value_type& val) {
-        node_pointer tmp = new node(val);
-        // tmp->content = val;
-        tmp->next = _begin;
-        _begin->prev = tmp;
-        _begin = tmp;
-        _size++;
+        _begin = insert(iterator(_begin), val).get_node();
     }
 
     void pop_front() {
         if (_size) {
-            node_pointer tmp = _begin;
-            _begin = _begin->next;
-            _begin->prev = NULL;
-            delete tmp;
-            _size--;
+            _begin = erase(begin()).get_node();
         }
     }
 
     void push_back (const value_type& val) {
-        if (_size == 0) {
-            _begin = new node(val);
-            _begin->next = _end;
-            _end->prev = _begin;
-        } else {
-            node_pointer tmp = new node(val);
-            tmp->next = _end;
-            tmp->prev = _end->prev;
-            tmp->prev->next = tmp;
-            _end->prev = tmp;
+        iterator it = insert(end(), val);
+        if (_size == 1) {
+            _begin = it.get_node();
         }
-        // _end->content = val;
-        _end->next = NULL;
-        _size++;
     }
 
     void pop_back() {
         if (_size) {
-            node_pointer tmp = _end->prev;
-            _end->prev = tmp->prev;
-            if (tmp->prev) {
-                tmp->prev->next = _end;
-            }
-            delete tmp;
-            _size--;
+            erase(--end());
         }
     }
-
+    
     iterator insert (iterator position, const value_type& val) {
-        node_pointer node = position.get_node();
+        node_pointer node_pos = position.get_node();
         node_pointer tmp = new node(val);
-        if (node->prev) {
-            node->prev->next = tmp;
+
+        if (node_pos->prev) {
+            node_pos->prev->next = tmp;
         }
-        tmp->prev = node->prev;
-        tmp->next = node;
-        node->prev = tmp;
+        tmp->prev = node_pos->prev;
+        tmp->next = node_pos;
+        node_pos->prev = tmp;
+        if (node_pos == _begin)
+            _begin = tmp;
+        _size++;
+        return (iterator(tmp));
     }
 
-    // void insert (iterator position, size_type n, const value_type& val);
+    void insert (iterator position, size_type n, const_reference val) {
+        for (size_type i = 0; i < n; i++) {
+            insert(position, val);
+        }
+    }
 
-    // template <class InputIterator>
-    // void insert (iterator position, InputIterator first, InputIterator last);
+    void insert (iterator position, const_iterator first, const_iterator last) {
+        while (first != last) {
+            insert(position, *first++);
+        }
+    }
+
+    iterator erase (iterator position) {
+        node_pointer node_pos = position.get_node();
+        if (node_pos == _end)
+            return (_end);
+        iterator ret = ++position;
+        if (node_pos->next) {
+            node_pos->next->prev = node_pos->prev;
+        }
+        if (node_pos->prev) {
+            node_pos->prev->next = node_pos->next;
+        }
+        if (node_pos == _begin) {
+            _begin = node_pos->next;
+        }
+        delete node_pos;
+        _size--;
+        return (ret);
+    }
+
+    iterator erase (iterator first, iterator last) {
+        while (first != last) {
+            erase(first++);
+        }
+        return (last);
+    }
+
+    void swap (List& x) {
+        node_pointer    sbegin = _begin;
+        node_pointer    send = _end;
+        size_type       ssize = _size;
+
+        _begin = x._begin;
+        _end = x._end;
+        _size = x._size;
+        x._begin = sbegin;
+        x._end = send;
+        x._size = ssize;
+    }
+
+    void resize (size_type n, value_type val = value_type()) {
+        if (n <= _size) {
+            for (size_type i = _size; i > n; i--) {
+                pop_back();
+            }   
+        } else {
+            insert(end(), n - _size, val);
+        }
+    }
+
+    void clear() {
+        erase(begin(), end());
+    }
+
+    void splice (iterator position, List& x) {
+            splice(position, x, x.begin(), x.end());
+    }
+
+    void splice (iterator position, List& x, iterator i) {
+        node_pointer node_pos = position.get_node();
+        node_pointer x_pos = i.get_node();
+        if (x_pos == x._end) {
+            return ;
+        }
+        if (x_pos->prev) {
+            x_pos->prev->next = x_pos->next;
+        }
+        if (x_pos->next) {
+            x_pos->next->prev = x_pos->prev;
+        }
+        if (node_pos->prev) {
+            node_pos->prev->next = x_pos;
+        }
+        if (x_pos == x._begin) {
+            x._begin = x_pos->next;
+        }
+        if (node_pos == _begin) {
+            _begin = x_pos;
+        }
+        x_pos->prev = node_pos->prev;
+        x_pos->next = node_pos;
+        node_pos->prev = x_pos;
+        _size++;
+        x._size--;
+    }
+
+    void splice (iterator position, List& x, iterator first, iterator last) {
+        while (first != last) {
+            splice(position, x, first++);
+        }
+    }
+
+    void remove (const value_type& val) {
+        node_pointer tmp = _begin;
+        while (tmp != _end) {
+            if (tmp->content == val) {
+                tmp = erase(tmp).get_node();
+            } else {
+                tmp = tmp->next;
+            }
+        }
+    }
+
+    template <class Predicate>
+    void remove_if (Predicate pred) {
+        node_pointer tmp = _begin;
+        while (tmp != _end) {
+            if (pred(tmp->content)) {
+                tmp = erase(tmp).get_node();
+            } else {
+                tmp = tmp->next;
+            }
+        }
+    }
+
+    void unique() {
+        node_pointer tmp = _begin;
+        while (tmp != _end) {
+            if (tmp->prev && tmp->content == tmp->prev->content) {
+                tmp = erase(tmp).get_node();
+            } else {
+                tmp = tmp->next;
+            }
+        }
+    }
+
+    template <class BinaryPredicate>
+    void unique (BinaryPredicate binary_pred) {
+        node_pointer tmp = _begin;
+        while (tmp != _end) {
+            if (tmp->prev && binary_pred(tmp->content, tmp->prev->content)) {
+                tmp = erase(tmp).get_node();
+            } else {
+                tmp = tmp->next;
+            }
+        }
+    }
+
+    void merge (List& x) {
+        splice(end(), x);
+    }
+
+    template <class Compare>
+    void merge (List& x, Compare comp) {
+        iterator it = begin();
+        value_type item = x._begin->content;
+        while (it != end() && !comp(item, *it))
+        {
+            ++it;
+        }
+        splice(it, x);
+    }
+
+    void sort() {
+        sort(operator<);
+    }
+
+    template <class Compare>
+    void sort (Compare comp) {
+        iterator it = begin();
+        iterator ite = end();
+        while (it != ite) {
+            if (comp(*it, *it->next))
+        }
+
+    }
+
+
+    // Operators Overload
+
 
 
 
@@ -361,7 +489,70 @@ private:
     node_pointer    _end;
     size_type       _size;
 
+    void    init() {
+        pointer val = static_cast<pointer>(::operator new(sizeof(value_type)));
+        _end = new node(*val);
+        _begin = _end;
+        _end->prev = _end->next = NULL;
+        _size = 0;
+        free(val);
+    }
+
 }; // List class
+
+
+template <class T>
+bool operator== (const List<T>& lhs, const List<T>& rhs) {
+    typename List<T>::iterator lhs_it = lhs.begin();
+    typename List<T>::iterator lhs_ite = lhs.end();
+    typename List<T>::iterator rhs_it = rhs.begin();
+    typename List<T>::iterator rhs_ite = rhs.end();
+    if (rhs._size != rhs._size)
+        return (false);
+    while (lhs_it != lhs_ite) {
+        if (*lhs_it++ != *rhs_it++)
+            return (false);
+    }
+    return (true);
+}
+
+template <class T>
+bool operator!= (const List<T>& lhs, const List<T>& rhs) {
+    return !(lhs == rhs);
+}
+
+template <class T>
+bool operator<  (const List<T>& lhs, const List<T>& rhs) {
+    typename List<T>::iterator lhs_it = lhs.begin();
+    typename List<T>::iterator lhs_ite = lhs.end();
+    typename List<T>::iterator rhs_it = rhs.begin();
+    typename List<T>::iterator rhs_ite = rhs.end();
+    while (lhs_it != lhs_ite && rhs_it != rhs_ite) {
+        if (*lhs_it < *rhs_it)
+            return (true);
+        lhs_it++;
+        rhs_it++;
+    }
+    if (lhs_it == lhs_ite)
+        return (true);
+    return (false);
+
+}
+
+template <class T>
+bool operator<= (const List<T>& lhs, const List<T>& rhs) {
+    return !(lhs > rhs);
+}
+
+template <class T>
+bool operator>  (const List<T>& lhs, const List<T>& rhs) {
+    return (rhs < lhs);
+}
+
+template <class T>
+bool operator>= (const List<T>& lhs, const List<T>& rhs) {
+    return !(lhs < rhs);
+}
 
 } // ft namespace
 
--- List/main.cpp
+++ List/main.cpp
@@ -1,6 +1,8 @@
 #include <list>
 #include <iostream>
 #include <iomanip>
+#include <vector>
+#include <cmath>
 #include "List.hpp"
 class A {
 public:
@@ -13,54 +15,129 @@ std::ostream & operator<<(std::ostream & out, A &a) {
     return (out << a._n);
 }
 
+bool is_even(const int i) {
+    return ((i % 2) == 0);
+}
+
+struct is_near {
+  bool operator() (double first, double second)
+  { return (std::fabs(first-second)<5.0); }
+};
+
+bool mycomparison (double first, double second)
+{ return ( int(first)<int(second) ); }
+
+
 int main(void) {
 
-    std::list<int> list;
+    int test = 2;
 
-    // list.push_back(2);
-    // list.pop_front();
-    // int a = list.back();
-    std::list<int>::iterator it = list.begin();
+    if (test == 1) {
+        std::list<int> list;
 
-    std::cout << *it << std::endl;
+        list.push_back(2);
+        list.push_back(2);
+        list.push_back(2);
+        list.push_back(6);
+        list.push_back(2);
+        list.push_back(8);
 
-    // ft::List<int> list;
+        list.unique();
+        std::list<int>::iterator it = list.begin();
+        std::list<int>::iterator ite = list.end();
+        while (it != ite) {
+            std::cout << "value: " << *it << std::endl;
+            it++;
+        }
+    } else if (test == 2) {
+        ft::List<double> first, second;
 
-    // int a = list.front();
+        first.push_back (1.1);
+        first.push_back (2.2);
+        first.push_back (2.9);
 
-    // std::cout << a << std::endl;
+        second.push_back (3.7);
+        second.push_back (7.1);
+        second.push_back (11.4);
 
-    // list.push_back(1);
-    // list.push_back(2);
-    // list.push_back(3);
-    // list.push_back(4);
-    // list.push_back(5);
-    // list.push_front(0);
+        // first.sort();
+        // second.sort();
 
-    // // list.pop_front();
+        first.merge(second);
 
-    // int a = list.back();
+        // (second is now empty)
+
+        second.push_back (7.0);
+
+        first.merge(second,mycomparison);
+
+        // ft::List<double>::iterator it = first.begin();
+        ft::List<double>::iterator ite = first.end();
+        std::cout << "first contains:";
+        for (ft::List<double>::iterator it=first.begin(); it!=ite; ++it)
+            std::cout << ' ' << *it;
+        std::cout << '\n';
+
+    } else if (test == 3) {
+
+        ft::List<int> list;
+        
+
+        // int a = list.front();
+
+        // std::cout << a << std::endl;
+
+        list.push_back(1);
+        list.push_back(2);
+        list.push_back(3);
+        list.push_back(4);
+        list.push_back(5);
+        list.push_front(0);
+
+        // list.pop_front();
+
+        int a = list.back();
 
 
-    // std::cout << ": " << a << std::endl;
+        std::cout << ": " << a << std::endl;
+        
+        ft::List<int>::iterator it = list.begin();
+        ft::List<int>::iterator ite = list.end();
+        ft::List<int> list2;
+
+        for (int i = 2; i < 200; i*=2) {
+            list2.push_back(i);
+        }
+
+        ft::List<int>::iterator it22 = list2.begin();
+
+        it22++;
+        list.splice(list.begin(), list2, list2.end());
+
+        it = list.begin();
+        ite = list.end();
+        while (it != ite) {
+            std::cout << "value1: " << *it << std::endl;
+            it++;
+        }
+
+        ft::List<int>::iterator it2 = list2.begin();
+        ft::List<int>::iterator ite2 = list2.end();
+        while (it2 != ite2) {
+            std::cout << "value2: " << *it2 << std::endl;
+            it2++;
+        }
+
+        // ++it;
+        // // (*it) = 7;
+        // std::cout << *it << std::endl;
+        // --it;
+        // std::cout << *it << std::endl;
+
+        std::cout << list2.size() << " " << list.size() << std::endl;
+        std::cout << std::boolalpha << list2.empty() << " " << list.empty() << std::endl;
+        std::cout << list2.max_size() << std::endl;
+    }
     
-    // ft::List<int>::reverse_iterator it = list.rbegin();
-    // ft::List<int>::reverse_iterator ite = list.rend();
-
-    // while (it != ite) {
-    //     std::cout << "value: " << *it << std::endl;
-    //     it++;
-    // }
-
-    // ++it;
-    // // (*it) = 7;
-    // std::cout << *it << std::endl;
-    // --it;
-    // std::cout << *it << std::endl;
-
-    std::cout << list.size() << std::endl;
-    std::cout << std::boolalpha << list.empty() << std::endl;
-    std::cout << list.max_size() << std::endl;
-
     return (0);
 }
